b63fbc0a5c79c4a1acc627582231836461dcb944
ky12453 <ky12453@gmail.com>
2020-01-09 07:08:57 +0900

    ARC096_Cをリファクタ

--- sandbox/tutorial/problem_8/ARC096_C.cpp
+++ sandbox/tutorial/problem_8/ARC096_C.cpp
@@ -5,16 +5,11 @@ int main() {
     int a, b, c, x, y;
     cin >> a >> b >> c >> x >> y;
 
-    int n = x;
-    if (x < y) n = y;
+    int n = max(x, y);
     long long int min = 1000000000000;
-    int a_num, b_num, total;
+    int total;
     for (int i = 0; i <= 2 * n; i = i + 2) {
-        a_num = x - i / 2;
-        b_num = y - i / 2;
-        if (a_num < 0) a_num = 0;
-        if (b_num < 0) b_num = 0;
-        total = a_num * a + b_num * b + i * c;
+        total = i * c + max(0, x - i / 2) * a + max(0, y - i / 2) * b;
         if (min > total) min = total;
     }
 
