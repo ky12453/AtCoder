451aea00d1e7c6318cc4cebad830ab7cdb7657c7
ky12453 <ky12453@gmail.com>
2020-01-12 09:58:44 +0900

    ABC057_Cをリファクタ

--- sandbox/tutorial/task_8/ABC057_C.cpp
+++ sandbox/tutorial/task_8/ABC057_C.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+using ll = long long;
 
-int f(long long int a, long long int b) {
-    int digit_a = 0;
-    int digit_b = 0;
+int count_digits(ll n) {
+    int digits = 0;
 
-    while (a > 0) {
-        digit_a++;
-        a = a / 10;
+    while (n > 0) {
+        digits++;
+        n /= 10;
     }
 
-    while (b > 0) {
-        digit_b++;
-        b = b / 10;
-    }
-
-    return max(digit_a, digit_b);
+    return digits;
 }
 
 int main() {
-    long long int n;
+    ll n;
     cin >> n;
 
     int min = 11;
-    for (int i = 1; i <= (int)sqrt(n); ++i) {
-        long long int j;
-        int t;
-        if (n % i == 0) j = n / i;
-        t = f(i, j);
-        if (t < min) min = t;
+    for (ll i = 1; i * i <= n; ++i) {
+
+        // nの約数ペアを算出する
+        if (n % i != 0) continue;
+        ll j = n / i;
+
+        // 最小桁数を更新する
+        int cur = max(count_digits(i), count_digits(j));
+        if (cur < min) min = cur;
     }
 
     cout << min << endl;
