03a1adbaaa9f8016956e0bc480301b7a03c12dfc
ky12453 <ky12453@gmail.com>
2020-01-15 22:09:11 +0900

    ABC088_Aを解答

---
+++ sandbox/tutorial/task_1/ABC088_A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int n, a;
+    cin >> n >> a;
+
+    string ans = "No";
+    if (n % 500 <= a) ans = "Yes";
+
+    cout << ans << endl;
+}
