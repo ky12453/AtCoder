ca291f89636724c0488f8314e6519d5fe6a55f88
ky12453 <ky12453@gmail.com>
2020-01-26 10:25:14 +0900

    ABC042_Bをリファクタ

--- sandbox/past_contests/ABC042/ABC042_B.cpp
+++ sandbox/past_contests/ABC042/ABC042_B.cpp
@@ -6,19 +6,14 @@ int main() {
     int n, l;
     cin >> n >> l;
 
-    string str;
-    vector<string> s;
-    for (int i = 0; i < n; ++i) {
-        cin >> str;
-        s.push_back(str);
-    }
+    vector<string> s(n);
+    for (int i = 0; i < n; ++i) cin >> s[i];
 
     sort(s.begin(), s.end());
 
     for (int i = 0; i < n; ++i) {
-        cout << s[i] ;
+        cout << s[i];
     }
-
     cout << endl;
     return 0;
 }
