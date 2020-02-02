aeaccc68cdff7bd8f2adcbde46864d9a4d6193e6
ky12453 <ky12453@gmail.com>
2020-02-02 10:38:50 +0900

    ディレクトリ構造を変更

---
+++ sandbox/past_contests/ABC044/ABC044_B.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main() {
+    string w;
+    cin >> w;
+
+    map<string, int> bucket;
+    for (int i = 0; i < w.length(); ++i) {
+        if (bucket.find(w[i]) == bucket.end()) {
+            bucket.insert(make_pair(s[i], 1));
+        } else {
+            bucket[w[i]] = bucket[w[i]] + 1;
+        }
+    }
+
+    string ans = "Yes";
+    for (auto itr = bucket.begin(); itr != bucket.end(); ++itr) {
+        int t = itr->second;
+        if (t % 2 != 0) ans = "No";
+    }
+
+    cout << ans << endl;
+    return 0;
+}
