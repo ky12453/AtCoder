06e56ebaffa3229daacb1d9654fcd0e31724e534
ky12453 <ky12453@gmail.com>
2020-02-13 23:46:30 +0900

    累積和の例題を解答

--- ABC/ABC154/task2.cpp
+++ ABC/ABC154/task2.cpp
@@ -9,12 +9,11 @@ int L[100010], R[100010];
 
 bool IsPrime(int p) {
     bool flg = true;
-    for (int i = 2; i <= (int)sqrt(p); ++i) {
+    for (int i = 2; i * i <= p; ++i) {
         if (p % i == 0) flg = false;
     }
 
     return flg;
-
 }
 
 int main() {
@@ -23,41 +22,31 @@ int main() {
 
     // Nまでの素数を取得
     // 素数の場合は、該当要素を1にする
-    vector<int> p(N, 0);
+    vector<int> p(N + 1, 0);
     for (int i = 2; i <= N; ++i) {
         if (IsPrime(i)) p[i] = 1;
     }
 
     // 2017に似た数を取得する
     // 似た数は、該当要素を2にする
+    vector<int> pp(N + 1, 0);
     for (int i = 2; i <= N; ++i) {
 
-        if (p[i] == 1) {
-            int t = (i + 1) / 2;
-
-            if (p[t] >= 1) {
-                p[i] = 2;
-            }
+        // 奇数を対象とする
+        if (i % 2 != 0) {
+            if (p[i] == 1 && p[(i + 1) / 2] == 1) pp[i] = 1;
         }
     }
 
     // 累積和を算出する
-    vector<int> s(N + 1, 0);
-    for (int i = 0; i < N; ++i) {
-        if (p[i] == 2) {
-            s[i + 1] = s[i] + 1;
-        } else {
-            s[i + 1] = s[i];
-        }
+    vector<int> s(N + 2, 0);
+    for (int i = 0; i <= N; ++i) {
+        s[i + 1] = s[i] + pp[i];
     }
 
     // 条件に合致する要素を検索する
     for (int i = 0; i < Q; ++i) {
-        if (L[i] == R[i]) {
-            cout << s[R[i] + 1] - s[L[i]] << endl;
-        } else {
-            cout << s[R[i]] - s[L[i]] << endl;
-        }
+        cout << s[R[i] + 1] - s[L[i]] << endl;
     }
 
     return 0;
