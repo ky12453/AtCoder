d73d7708429f9001cc777799e6f45e3559c7b897
ky12453 <ky12453@gmail.com>
2020-02-19 00:07:29 +0900

    誤解法を修正

--- sandbox/tutorial_dp/task_5.cpp
+++ sandbox/tutorial_dp/task_5.cpp
@@ -1,32 +1,36 @@
 #include <iostream>
 using namespace std;
 
+const int INF = 1<<29;
+
 int N, A;
-int B[10010];
+int B[110];
 
 int main() {
     cin >> N >> A;
     for (int i = 0; i < N; ++i) cin >> B[i];
 
-    int dp[110][10010] = {0};
+    // dp配列を初期化
+    int dp[110][10010];
+    for (int i = 0; i < 110; ++i) {
+        for (int j = 0; j < 10010; ++j) {
+            dp[i][j] = INF;
+        }
+    }
+    dp[0][0] = 0;
+
+    // dp配列を緩和
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j <= A; ++j) {
-            if (j >= B[i]) {
-                dp[i + 1][j] = max(dp[i + 1][j], dp[i][j]);
-                dp[i + 1][j] = max(dp[i + 1][j], dp[i][j - B[i]] + 1);
-            } else {
-                dp[i + 1][j] = max(dp[i + 1][j], dp[i][j]);
-            }
+            dp[i + 1][j] = min(dp[i + 1][j], dp[i][j]);
+            if (j >= B[i]) dp[i + 1][j] = min(dp[i + 1][j], dp[i][j - B[i]] + 1);
         }
     }
 
-    int ans = 100;
-    for (int i = 0; i <= N; ++i) {
-        if ( dp[i][A] >= 1) {
-            ans = min(ans, dp[i][A]);
-        }
+    if (dp[N][A] == INF) {
+        cout << -1 << endl;
+    } else {
+        cout << dp[N][A] << endl;
     }
-
-    cout << ans << endl;
     return 0;
 }
