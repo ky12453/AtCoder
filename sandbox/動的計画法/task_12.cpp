4ea47dba0f16ceb69ca739e042a8017b3a85cfba
ky12453 <ky12453@gmail.com>
2020-02-27 23:10:04 +0900

    Educational DP Contest E問題を解答

---
+++ sandbox/動的計画法/task_12.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+const long long INF = 1LL<<60;
+long long dp[110][100010];
+
+int main() {
+    int N, W;
+    cin >> N >> W;
+
+    int weight[110];
+    int value[110];
+    for (int i = 0; i < N; ++i) cin >> weight[i] >> value[i];
+
+    // dp配列を初期化
+    for (int i = 0; i < 110; ++i) {
+        for (int j = 0; j < 100010; ++j) {
+            dp[i][j] = INF;
+        }
+    }
+    dp[0][0] = 0;
+
+    // dp配列を緩和
+    for (int i = 1; i <= N; ++i) {
+        for (int j = 0; j <= 100000; ++j) {
+            dp[i][j] = min(dp[i][j], dp[i - 1][j]);
+            if (j >= value[i - 1]) {
+                if (dp[i - 1][j - value[i - 1]] + weight[i - 1] <= W) {
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j - value[i - 1]] + weight[i - 1]);
+                }
+            }
+        }
+    }
+
+    // 価値の最大値を探索
+    int ans = 0;
+    for (int j = 0; j <= 100000; ++j) {
+        if (dp[N][j] != INF) {
+            ans = max(ans, j);
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}
