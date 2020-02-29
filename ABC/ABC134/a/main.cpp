46404b8e03eb4a6dcac408ff044301c04395cd96
ky12453 <ky12453@gmail.com>
2020-02-29 23:02:51 +0900

    ABC134_Aを解答

---
+++ ABC/ABC134/a/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int R;
+    cin >> R;
+
+    cout << 3 * R * R << endl;
+    return 0;
+}
