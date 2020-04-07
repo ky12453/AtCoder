e71dc325f8ec65be55916b8778ce869352b41170
ky12453 <ky12453@gmail.com>
2020-04-07 23:58:39 +0900

    ワーシャルフロイド法を実装

---
+++ sandbox/TextBook/Chapter1/05_グラフ/5.cpp
@@ -0,0 +1,36 @@
+// 蟻本P98
+// ワーシャルフロイド法を実装
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int INF = 1 << 29;
+const int MAX_V = 20;
+
+int main() {
+    int V, E; cin >> V >> E;
+    int S, G; cin >> S >> G;
+
+    vector<vector<int>> graph(MAX_V, vector<int>(MAX_V, INF));
+    for (int i = 0; i < V; ++i) {
+        graph[i][i] = 0;
+    }
+
+    for (int i = 0; i < E; ++i) {
+        int f, t, c; cin >> f >> t >> c;
+        f--; t--;
+        graph[f][t] = min(graph[f][t], c);
+        graph[t][f] = min(graph[t][f], c);       
+    }
+
+    for (int k = 0; k < V; ++k) {
+        for (int i = 0; i < V; ++i) {
+            for (int j = 0; j < V; ++j) {
+                graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][i]);
+            }
+        }
+    }
+
+    cout << graph[S][G] << endl;
+    return 0;
+}
