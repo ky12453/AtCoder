10137f4de564e04a32234f01fd1cbf5d4e52372f
ky12453 <ky12453@gmail.com>
2020-04-13 23:24:53 +0900

    ABC162_Dをリファクタ

--- ABC/ABC162/ABC162_D.cpp
+++ ABC/ABC162/ABC162_D.cpp
@@ -6,71 +6,24 @@ int main() {
     int N; cin >> N;
     char S[N]; for (int i = 0; i < N; ++i) cin >> S[i];
 
-    int r[N], g[N], b[N];
-    int sr = 0;
-    int sg = 0;
-    int sb = 0;
+    ll r = 0, g = 0, b = 0;
     for (int i = 0; i < N; ++i) {
-        if (S[i] == 'R') sr++;
-        if (S[i] == 'G') sg++;
-        if (S[i] == 'B') sb++;
-        r[i] = sr;
-        g[i] = sg;
-        b[i] = sb;
+        if (S[i] == 'R') r++;
+        if (S[i] == 'G') g++;
+        if (S[i] == 'B') b++;
     }
 
-    ll res = 0;
-    for (int i = 0; i < N - 2; ++i) {
-        char refi = S[i];
-        for (int j = i + 1; j < N - 1; ++j) {
-            char refj = S[j];
-            if (refj != refi) {
-                if (refi == 'R') {
-                    if (refj == 'G') {
-                        res += sb - b[j];
-                        int t = 2 * j - i;
-                        if (t < N) {
-                            if (S[t] == 'B') res--;
-                        }
-                    } else {
-                        res += sg - g[j];
-                        int t = 2 * j - i;
-                        if (t < N) {
-                            if (S[t] == 'G') res--;
-                        }
-                    }
-                } else if (refi == 'G') {
-                    if (refj == 'B') {
-                        res += sr - r[j];
-                        int t = 2 * j - i;
-                        if (t < N) {
-                            if (S[t] == 'R') res--;
-                        }
-                    } else {
-                        res += sb - b[j];
-                        int t = 2 * j - i;
-                        if (t < N) {
-                            if (S[t] == 'B') res--;
-                        }
-                    }
-                } else if (refi == 'B') {
-                    if (refj == 'R') {
-                        res += sg - g[j];
-                        int t = 2 * j - i;
-                        if (t < N) {
-                            if (S[t] == 'G') res--;
-                        }
-                    } else {
-                        res += sr - r[j];
-                        int t = 2 * j - i;
-                        if (t < N) {
-                            if (S[t] == 'R') res--;
-                        }
-                    }
-                }
-            }
+    ll ans = r * g * b;
+    
+    for (int i = 0; i < N; ++i) {
+        for (int j = i + 1; j < N; ++j) {
+            if (S[i] == S[j]) continue;
+
+            int k = j * 2 - i;
+            if (k < N && S[i] != S[k] && S[j] != S[k]) ans--;
         }
     }
-    cout << res << endl;
+
+    cout << ans << endl;
     return 0;
 }
