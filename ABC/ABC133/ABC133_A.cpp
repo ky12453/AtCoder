6183a724a63fd718743f9f3e0d1afe0804e07da3
ky12453 <ky12453@gmail.com>
2020-04-25 23:52:53 +0900

    ABC133_Aを解答

--- ABC/ABC133/ABC133_A.cpp
+++ ABC/ABC133/ABC133_A.cpp
@@ -1,10 +1,8 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int N, A, B;
-    cin >> N >> A >> B;
-
-    cout << min(A * N, B) << endl;
+    int n, a, b; cin >> n >> a >> b;
+    cout << min(a * n, b) << endl;
     return 0;
 }
