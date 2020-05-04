05ee05c51b7f9af1afe7c26123aaa95059188590
ky12453 <ky12453@gmail.com>
2020-05-04 12:04:13 +0900

    ABC137_Bをリファクタ

--- ABC/ABC137/ABC137_B.cpp
+++ ABC/ABC137/ABC137_B.cpp
@@ -3,11 +3,6 @@ using namespace std;
 
 int main() {
     int k, x; cin >> k >> x;
-    int l = max(x - k + 1, -1000000);
-    int r = min(x + k - 1, 1000000);
-    for (int i = l; i <= r; ++i) {
-        cout << i << " ";
-    }
+    for (int i = x - k + 1; i <= x + k - 1; ++i) cout << i << " ";
     cout << endl;
-    return 0;
 }
