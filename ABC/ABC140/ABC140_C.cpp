5233df0e3fe99a8d2856c8ecd00231c64ee0847c
ky12453 <ky12453@gmail.com>
2020-05-20 00:18:05 +0900

    ABC140_Cを解答

--- ABC/ABC140/ABC140_C.cpp
+++ ABC/ABC140/ABC140_C.cpp
@@ -1,20 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-
 #define REP(w, n) for(int w=0;w<int(n);++w)
+
+const int INF = 1<<29;
 
 int main() {
     int n; cin >> n;
-    vector<int> b(n + 2, 0);
+    vector<int> b(n + 2, INF);
     for (int i = 1; i < n; ++i) cin >> b[i];
-    b[0] = b[1];
-    b[n] = b[n - 1];
-    b[n + 1] = b[n];
 
     int res = 0;
-    for (int i = 1; i <= n; ++i) {
-        res += min({b[i - 1], b[i], b[i + 1]});
-    }
+    for (int i = 1; i <= n; ++i) res += min(b[i - 1], b[i]);
 
     cout << res << endl;
 }
