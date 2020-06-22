4d91433479788891efd6ae780ea07fc212704b77
ky12453 <ky12453@gmail.com>
2020-06-22 22:35:17 +0900

    ABC143を解答

---
+++ ABC/ABC143/ABC143_A.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int a, b; cin >> a >> b;
+  if (a > 2 * b) {
+    cout << a - (2 * b) << endl;
+  } else {
+    cout << 0 << endl;
+  }
+}
